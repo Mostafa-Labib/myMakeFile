bb9a3f9419b76b86651dedd38bce3418fa6a177a
medineeds <medineedsrand@gmail.com>
2021-02-23 16:01:09 +0200

    chanes in help.cpp and help.hpp

--- help.cpp
+++ help.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-
+void hello_america(void)
+{
+    
+}
 void print(void)
 {
     std::cout << "hey mostafa" <<std::endl;
--- help.hpp
+++ help.hpp
@@ -1,6 +1,6 @@
 #ifndef HELP_H_
 #define HELP_H_
-
+void hello_america(void);
 void print(void);
 
 #endif
